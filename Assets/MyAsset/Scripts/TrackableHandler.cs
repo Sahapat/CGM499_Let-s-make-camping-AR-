7e5e4b0f1087aad48f915f57fc18eb0380474f47
Sahapat <sahapat.starcaft@gmail.com>
2018-02-25 22:23:45 +0700

    Handle Trakable event from vuforia

---
+++ Assets/MyAsset/Scripts/TrackableHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackableHandler : DefaultTrackableEventHandler
+{
+    public TargetTag myTag;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        Main.instance.setTargetStatus(myTag, true);
+        this.GetComponent<MeshRenderer>().enabled = false;
+    }
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        Main.instance.setTargetStatus(myTag, false);
+    }
+}
